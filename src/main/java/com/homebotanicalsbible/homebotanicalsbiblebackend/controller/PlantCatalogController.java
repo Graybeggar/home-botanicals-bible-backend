b3c188a72d23eb8974fa225d08b8f5fe1db03459
Graybeggar <blhat@hotmail.com>
2025-07-16 12:31:39 -0600

    PlantCatalogController - entity/model

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
@@ -1,2 +1,5 @@
 // Defines the package
 package com.homebotanicalsbible.homebotanicalsbiblebackend.controller;
+
+// Import the PlantCatalog entity/model class
+import com.homebotanicalsbible.homebotanicalsbiblebackend.model.PlantCatalog;
