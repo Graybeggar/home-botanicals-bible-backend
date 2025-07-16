4f5c4a3675e6e61e511ac908520624596f7a3a07
Graybeggar <blhat@hotmail.com>
2025-07-16 12:33:49 -0600

    PlantCatalogController - import annotation inject dependencies

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
@@ -6,3 +6,6 @@ import com.homebotanicalsbible.homebotanicalsbiblebackend.model.PlantCatalog;
 
 // Import the repository interface that provides database access methods for PlantCatalog
 import com.homebotanicalsbible.homebotanicalsbiblebackend.repository.PlantCatalogRepository;
+
+// Import annotation to allow Spring to automatically inject dependencies
+import org.springframework.beans.factory.annotation.Autowired;
