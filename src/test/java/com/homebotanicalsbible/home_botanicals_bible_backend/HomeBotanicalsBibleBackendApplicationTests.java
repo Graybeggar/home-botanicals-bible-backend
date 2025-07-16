072a8c9e90b762a4a8526f57990f480bbb53148b
Graybeggar <blhat@hotmail.com>
2025-07-16 08:10:48 -0600

    initial commit

---
+++ src/test/java/com/homebotanicalsbible/home_botanicals_bible_backend/HomeBotanicalsBibleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homebotanicalsbible.home_botanicals_bible_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBotanicalsBibleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
