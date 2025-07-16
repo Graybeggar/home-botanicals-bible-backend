a4b077248c170537b33736ce3ce23e02335a7ac2
Graybeggar <blhat@hotmail.com>
2025-07-16 14:39:03 -0600

    GardenPlantModel - import lombok

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/model/GardenPlant.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/model/GardenPlant.java
@@ -1,2 +1,5 @@
 // Import JPA annotations like @Entity, @Id, @GeneratedValue, @ManyToOne, @JoinColumn, etc.
 import jakarta.persistence.*;
+
+// Import Lombok annotations to automatically generate boilerplate code (getters, setters, constructors)
+import lombok.*;
