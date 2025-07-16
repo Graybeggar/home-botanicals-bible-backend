9362f8b2d2f6abe9539533ebff0354163ba19df3
Graybeggar <blhat@hotmail.com>
2025-07-16 08:33:20 -0600

    GardenPlantController - import GardenPlant Model

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
@@ -1,2 +1,5 @@
 // Package declaration: this class is part of the controller package
 package com.homebotanicalsbible.homebotanicalsbiblebackend.controller;
+
+// Import the GardenPlant model class to use as a data type
+import com.homebotanicalsbible.homebotanicalsbiblebackend.model.GardenPlant;
