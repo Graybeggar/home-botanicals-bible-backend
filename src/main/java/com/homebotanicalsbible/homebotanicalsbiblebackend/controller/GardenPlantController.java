07472ad9d65093b667030d7169ca0f5db8cc8a51
Graybeggar <blhat@hotmail.com>
2025-07-16 08:40:40 -0600

    GardenPlantController - import JPA

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
@@ -3,3 +3,6 @@ package com.homebotanicalsbible.homebotanicalsbiblebackend.controller;
 
 // Import the GardenPlant model class to use as a data type
 import com.homebotanicalsbible.homebotanicalsbiblebackend.model.GardenPlant;
+
+// Import the JPA repository interface to perform DB operations on GardenPlant entities
+import com.homebotanicalsbible.homebotanicalsbiblebackend.repository.GardenPlantRepository;
