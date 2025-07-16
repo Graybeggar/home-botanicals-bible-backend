e702083309833168312ab491897f22dc326c54de
Graybeggar <blhat@hotmail.com>
2025-07-16 12:32:43 -0600

    PlantCatalogController - import repository

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/PlantCatalogController.java
@@ -3,3 +3,6 @@ package com.homebotanicalsbible.homebotanicalsbiblebackend.controller;
 
 // Import the PlantCatalog entity/model class
 import com.homebotanicalsbible.homebotanicalsbiblebackend.model.PlantCatalog;
+
+// Import the repository interface that provides database access methods for PlantCatalog
+import com.homebotanicalsbible.homebotanicalsbiblebackend.repository.PlantCatalogRepository;
