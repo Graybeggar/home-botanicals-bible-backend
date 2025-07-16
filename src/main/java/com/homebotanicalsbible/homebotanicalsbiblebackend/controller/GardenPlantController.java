93d7abd22113754abe810bc8f559e4d3fbe1e467
Graybeggar <blhat@hotmail.com>
2025-07-16 08:46:54 -0600

    GardenPlantController - spring annotation

--- src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
+++ src/main/java/com/homebotanicalsbible/homebotanicalsbiblebackend/controller/GardenPlantController.java
@@ -6,3 +6,6 @@ import com.homebotanicalsbible.homebotanicalsbiblebackend.model.GardenPlant;
 
 // Import the JPA repository interface to perform DB operations on GardenPlant entities
 import com.homebotanicalsbible.homebotanicalsbiblebackend.repository.GardenPlantRepository;
+
+// Spring annotation that allows dependency injection (@Autowired) and other core features
+import org.springframework.beans.factory.annotation.Autowired;
